791ae813e9372d83a3c7551085aafb993457e143
Erick Artur Garcia <erick.garcia@havan.com.br>
2023-05-02 20:33:34 -0300

    test26

--- WriteDownOnlineApi/Program.cs
+++ WriteDownOnlineApi/Program.cs
@@ -17,10 +17,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddMediatR(typeof(CreateUserHandler).Assembly);
 
-string mysqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
-string test = "server=containers-us-west-87.railway.app;port=6759;database=railway;user=root;password=kjesSp24aqrP7h8i7Uyh;Persist Security Info=False; Connect Timeout=300";
+string mysqlConnection = builder.Configuration.GetConnectionString("ProdConnection");
 builder.Services.AddDbContext<DbContext>(
-    options => options.UseMySql(test, ServerVersion.AutoDetect(test)));
+    options => options.UseMySql(mysqlConnection, ServerVersion.AutoDetect(mysqlConnection)));
 
 var app = builder.Build();
 
